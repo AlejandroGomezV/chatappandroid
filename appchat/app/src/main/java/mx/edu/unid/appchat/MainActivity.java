5c3af8a887e7783b1710dda724b4518308c043d6
Alejandro Gomez <m.alejandro.gomez.v@gmail.com>
2020-05-30 14:05:27 -0500

    prueba subir cambios
    prueba de actualizacion de cambios Alex

--- appchat/app/src/main/java/mx/edu/unid/appchat/MainActivity.java
+++ appchat/app/src/main/java/mx/edu/unid/appchat/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //comentario Alex
+    //Agreguen aqui un comentario y suban el cambio para saber que ya pueden contribuir
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
