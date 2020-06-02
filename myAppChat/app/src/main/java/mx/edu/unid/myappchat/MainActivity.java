d0d5238ee0ac21c8a462bb856f55b79472b64216
Alejandro Gomez <m.alejandro.gomez.v@gmail.com>
2020-06-01 23:45:45 -0500

    Toolbar
    Se agrego la barra con menu desplegable

--- myAppChat/app/src/main/java/mx/edu/unid/myappchat/MainActivity.java
+++ myAppChat/app/src/main/java/mx/edu/unid/myappchat/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import java.util.ArrayList;
@@ -25,6 +28,36 @@ public class MainActivity extends AppCompatActivity {
 
         adaptadorConversacion = new RecyclerViewAdaptador(obtenerConversaciones());
         recyclerViewConversacion.setAdapter(adaptadorConversacion);
+    }
+
+    //metodo para mostrar y ocultar el menu
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.overflow, menu);
+        return true;
+    }
+
+    //metodo para asignar las funciones correspondientes a las opciones
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.item1){
+            Toast.makeText(this,"Opcion 1", Toast.LENGTH_SHORT).show();
+            return  true;
+        } else if (id == R.id.item2) {
+            Toast.makeText(this,"Opcion 2", Toast.LENGTH_SHORT).show();
+            return  true;
+        } else if (id == R.id.item3){
+            Toast.makeText(this,"Opcion 3", Toast.LENGTH_SHORT).show();
+            return  true;
+        } else if (id == R.id.item4){
+            Toast.makeText(this,"Opcion 4", Toast.LENGTH_SHORT).show();
+            return  true;
+        } else if (id == R.id.item5){
+            Toast.makeText(this,"Opcion 5", Toast.LENGTH_SHORT).show();
+            return  true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public List<ConversacionModelo> obtenerConversaciones(){
